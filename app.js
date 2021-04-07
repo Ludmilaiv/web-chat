a413b6ba27298eda4a62eab033efc86d7cfe8e8e
Ludmilaiv <ludiky30@gmail.com>
2021-04-08 02:27:22 +0700

    part-4

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 
 var app = express();
 
+// var logger = require('morgan');
+// app.use(logger('request processed: :date[web] :method :url :status - :response-time ms'));
+
 app.get("/",function(req, res) {
   res.end("Hello");
 })
@@ -15,9 +18,9 @@ app.use("/forbidden",function(req, res, next) {
   next(createError(500, "Woops! You can't come here"))
 })
 
-app.use("/favicon.ico",function(req, res) {
-  res.sendStatus(200);
-})
+// app.use("/favicon.ico",function(req, res) {
+//   res.sendStatus(200);
+// })
 
 app.use(function(req, res) {
   res.status(404);
@@ -26,7 +29,7 @@ app.use(function(req, res) {
 
 // var path = require('path');
 // var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -37,7 +40,6 @@ var logger = require('morgan');
 // app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
